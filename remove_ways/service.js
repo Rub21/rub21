a4eb6c85cee54e9f2621889b2b56229e49f22ba3
Rub21 <ruben@mapbox.com>
2015-07-30 16:54:58 -0500

    crom job

--- remove_ways/service.js
+++ remove_ways/service.js
@@ -3,6 +3,8 @@ var _ = require('underscore');
 var parser = require('xml2json');
 var request = require('request');
 var fs = require('fs');
+var crontab = require('node-crontab');
+
 var user = process.env.DBUsername || 'postgres';
 var password = process.env.DBPassword || '';
 var address = process.env.DBAddress || 'localhost';
@@ -19,7 +21,7 @@ pg.connect(conString, function(err, c, d) {
 	console.log('connected to:', address);
 	client = c;
 });
-var i = 0;
+
 
 var options = {
 	object: true,
@@ -31,15 +33,18 @@ var options = {
 };
 
 var highway = ['footway', 'path', 'track', 'cycleway', 'bridleway', 'steps'];
-setTimeout(function() {
-	var query = 'SELECT key, value, "time"  FROM highwayhighway WHERE "time" = 0 limit 50;';
+var nums = [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000, 900000, 1000000, 1100000, 1200000, 1300000, 1400000, 1435887];
+var count = 1;
+function init(limit, offset) {
+	var i = 0;
+	var query = 'SELECT key, value, "time"  FROM highwayhighway WHERE "time" = 0 limit 100000 offset ' + offset + ';';
+	console.log(query);
 	var cliente = client.query(query, function(err, results) {
 		results.rows.forEach(function(row) {
 			var item = JSON.parse(row.value.split('|').join('"'));
 			//var url = 'http://overpass-api.de/api/interpreter?data=way(' + item.object_id + ');out;';
-			var url = 'http://overpass.osm.rambler.ru/cgi/interpreter?data=way(' + item.object_id + ');out;';
-			//var url = 'http://www.openstreetmap.org/api/0.6/way/'+ item.object_id + '/history';
-			console.log(url)
+			//var url = 'http://overpass.osm.rambler.ru/cgi/interpreter?data=way(' + item.object_id + ');out;';
+			var url = 'http://www.openstreetmap.org/api/0.6/way/' + item.object_id + '/history';
 			request(url, function(error, response, body) {
 				if (!error && response.statusCode == 200) {
 					var json = parser.toJson(body, options);
@@ -48,7 +53,7 @@ setTimeout(function() {
 						_.map(json.osm.way.tag, function(obj) {
 							if (obj.k === 'highway' && _.indexOf(highway, obj.v) >= 0) {
 								console.log(obj.v);
-								var q = 'UPDATE highwayhighway SET "time"= 2147483646 WHERE key = \'' + row.key + '\';\n';
+								var q = 'DELETE FROM highwayhighway WHERE key = \'' + row.key + '\';\n';
 								fs.appendFile('query.sql', q, function(err) {});
 							}
 						});
@@ -58,5 +63,16 @@ setTimeout(function() {
 				}
 			});
 		});
+	});
+}
+
+setTimeout(function() {
+	crontab.scheduleJob("0 */3 * * * ", function() {
+		if (count < 14) {
+			init(nums[count + 1], nums[count]);
+			count++;
+		} else {
+			console.log("Done!")
+		}
 	});
 }, 3000);
