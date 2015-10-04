2b030fb5249fa861e8a6c7a618eff19d6a0a88a0
Rub21 <ruben@mapbox.com>
2015-10-04 00:51:25 -0500

    mensaje

--- chat/server.js
+++ chat/server.js
@@ -12,15 +12,37 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
 	console.log(socket.id);
+	//io.emit() llega a todos los usuarios incluyendo a nosostros
 
-	io.emit('message', 'El usuario ' + socket.id + ' se ha conectado : ', 'System');
+	var channel = "channel-a"; //cannal por defecto
+
+	socket.broadcast.emit('message', 'El usuario ' + socket.id + ' se ha conectado : ', 'System'); //
+
+	socket.join(channel);
+
 	socket.on('message', function(msj) {
-		io.emit('message', msj, socket.id);
+		//io.emit('message', msj, socket.id); // emite a todos
+		io.sockets.in(channel).emit("message",msj,socket.id);//envia al cannal
+		//socket.broadcast.to(channel).emit("message",msj,socket.id); //envia a todos menos a mi
+
+
 	});
+
 	socket.on('disconnect', function(e) {
 		socket.emit('message', socket.id);
 
 	});
+
+	//cambiamos de canal
+
+	socket.on("change_channel", function(newChannel) {
+		socket.leave(channel);
+		socket.join(newChannel);
+		channel= newChannel;
+		socket.emit('change_channel',newChannel)
+
+	});
+
 });
 
 
