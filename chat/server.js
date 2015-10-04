c12231657d4dacf831854c84a820cf4ed2654709
Rub21 <ruben@mapbox.com>
2015-10-03 21:33:32 -0500

    mesnaje con styles

--- chat/server.js
+++ chat/server.js
@@ -13,6 +13,7 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket) {
 	console.log(socket.id);
 
+	io.emit('message', 'El usuario ' + socket.id + ' se ha conectado : ', 'System');
 	socket.on('message', function(msj) {
 		io.emit('message', msj, socket.id);
 	});
