0a61946df58b519bbc72abf4bc9ab3129b3fc9e2
Rub21 <ruben@mapbox.com>
2015-11-09 09:11:04 -0500

    alert dialog

--- rub21android/Apptest/app/src/main/java/rub21/apptest/MainActivity.java
+++ rub21android/Apptest/app/src/main/java/rub21/apptest/MainActivity.java
@@ -1,6 +1,9 @@
 package rub21.apptest;
 
 import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.media.Rating;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,27 +11,54 @@ import android.view.Menu;
 import android.view.MenuItem;
 import  android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+
+    public static Button button_sbm;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onButtonClickListner();
+        
     }
 
+public void onButtonClickListner(){
+    button_sbm= (Button) findViewById(R.id.button);
+    button_sbm.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            AlertDialog.Builder a_buBuilder = new AlertDialog.Builder(MainActivity.this);
+            a_buBuilder.setMessage("Do you want anything?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
 
-    public  void onButtonClick(View v){
-        EditText e1 = (EditText)findViewById(R.id.editText);
-        EditText e2 = (EditText)findViewById(R.id.editText2);
-        TextView t1 = (TextView)findViewById(R.id.textView);
+            AlertDialog alert= a_buBuilder.create();
+            alert.setTitle("Alert!!");
+            alert.show();
 
-        int num1 = Integer.parseInt(e1.getText().toString());
-        int num2 = Integer.parseInt(e2.getText().toString());
-        int sum = num1+num2;
-        t1.setText(Integer.toString(sum));
-    }
+
+        }
+    });
+}
 
 }
