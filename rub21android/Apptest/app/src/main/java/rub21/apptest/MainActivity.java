9bdc77ae5f9edf698492d1d07def1009a0ac783d
Rub21 <ruben@mapbox.com>
2015-11-09 07:57:11 -0500

    sum

--- rub21android/Apptest/app/src/main/java/rub21/apptest/MainActivity.java
+++ rub21android/Apptest/app/src/main/java/rub21/apptest/MainActivity.java
@@ -7,79 +7,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import  android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
-    public  static  final String MY_TAG= "the_custom_message";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i(MY_TAG,"onCreate");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(MY_TAG, "onStart");
     }
 
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(MY_TAG, "onResume");
+    public  void onButtonClick(View v){
+        EditText e1 = (EditText)findViewById(R.id.editText);
+        EditText e2 = (EditText)findViewById(R.id.editText2);
+        TextView t1 = (TextView)findViewById(R.id.textView);
+
+        int num1 = Integer.parseInt(e1.getText().toString());
+        int num2 = Integer.parseInt(e2.getText().toString());
+        int sum = num1+num2;
+        t1.setText(Integer.toString(sum));
     }
 
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(MY_TAG, "onPause");
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(MY_TAG, "onStop");
-    }
-
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i(MY_TAG, "onRestart");
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(MY_TAG, "onDestroy");
-    }
-
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
